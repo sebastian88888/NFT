8da80f4ddff5e1ebf51e5a2c7acbaf75e603f493
sebastian88888 <sebastian_eitelbuss@t-online.de>
2022-08-02 16:12:30 +0200

    page style

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,12 +1,12 @@
 const contractAddress = "0xb551bB27A8d7127Be59255A486873bC285F53E00"; // Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby, polygon, or ethereum
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h1 = "Welcome to the Quirky Quokka Globetrotters NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
-const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
-const h1_presale_mint = "Pre-Sale Minting Open!!";
-const h1_public_mint = "Public Minting Open!!";
+const welcome_p = 'The Quirky Quokka Globetrotters may live in Australia, but they love to travel around the world. To find out which country your Quirky Quokkas are currently in, mint one and check the properties of your NFT. How far have your Quirky Quokkas travelled?';
+const h1_presale_coming_soon = "NFT Drop Coming Soon!";
+const h1_presale_mint = "Pre-Sale Minting Open!";
+const h1_public_mint = "Public Minting Open!";
 const h2_presale_coming_soon = "Pre-Sale Minting Countdown";
 const h2_presale_mint = "Public Minting Countdown";
 const p_presale_coming_soon = "We are working hard to launch the NFT Collection. Stay tuned for updates!";
