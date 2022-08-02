1e8d8263fdaef5bfacfa12f3a75731d7e67ee390
sebastian88888 <sebastian_eitelbuss@t-online.de>
2022-08-02 16:31:06 +0200

    deactivate spinner

--- backend/src/config.js
+++ backend/src/config.js
@@ -62,7 +62,7 @@ const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a singl
 const PUBLIC_MINT_START_DATE = "2022-09-20T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-08-01T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 800; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x5b2FfacA16748Cad1b807c1e0cF4Dc8F0A0455F9"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
